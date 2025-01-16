1799035b823df4131dcc210107db578e4139103d
rodrigo oliveira <pracimaestudos@gmail.com>
2025-01-16 12:26:21 -0300

    Main

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Home from './pages/home/Home'
+import Contador from './pages/home/Contador'
+
+const App = () => {
+  return (
+   
+<>
+
+<Home/>
+<Contador/>
+</>
+
+
+
+
+  )
+}
+
+export default App
